4e6fafb1cc96f2d71bc329d28971e81a333db5fc
TF <122116776+ThomasFry@users.noreply.github.com>
2023-04-28 15:58:40 +0100

    Add files via upload

---
+++ foot.js
@@ -0,0 +1,17 @@
+let footerthin = `
+
+<section class="footer-thin">
+        <div class="row">
+            <div class="footer-thin-left col-lg-6">
+                <p>© 2023 Faithway College </p>
+            </div>
+            <div class="footer-thin-right col-lg-6">
+                <p>Tel: (+27) 33 7021257 <a href="https://www.facebook.com/faithway.collegehimeville/"><i class="fa-brands fa-square-facebook"></i></a></p>
+            </div>
+
+        </div>
+
+    </section>
+    `;
+
+    document.getElementById("footer-thin").innerHTML = footerthin
